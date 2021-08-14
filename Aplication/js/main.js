73e4a5fd3c3b4566e67e618a5f414fcd6f809cf6
João Marcos <joaomarcossandre@hotmail.com>
2021-08-14 16:50:28 -0300

    ADD aplicação ao REP git

---
+++ Aplication/js/main.js
@@ -0,0 +1,7 @@
+function acessarUsuario(){
+    user = {usuario, password};
+    user.usuario = document.getElementById("login").inputMode();
+
+    var userType;
+    window.location.href = "http://"+(userType)
+}
